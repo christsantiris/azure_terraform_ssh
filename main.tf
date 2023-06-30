da89a1eb2463f0f038ab7181ac35d20607f3775b
Chris Tsantiris <chris.tsantiris@gmail.com>
2023-06-30 16:28:05 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,165 @@
+# set up the azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# create the resource group
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "East Us"
+  tags = {
+    environment = "dev"
+  }
+}
+
+# create virtual network
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"] # cidr block
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# create subnet
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+# create security group
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location # location in resource group
+  resource_group_name = azurerm_resource_group.mtc-rg.name     #resource group name
+  tags = {
+    environment = "dev"
+  }
+}
+
+# create network security rule
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*" # should be your whitelisted ip(s)
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
+
+# associate security group to subnet
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
+}
+
+# give resource group a public ip
+resource "azurerm_public_ip" "mtc-ip" {
+  name                    = "mtc-ip"
+  location                = azurerm_resource_group.mtc-rg.location
+  resource_group_name     = azurerm_resource_group.mtc-rg.name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# add network interface
+resource "azurerm_network_interface" "mtc-nic" {
+  name                = "mtc-nic"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# add vm and allow ssh
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                = "mtc-vm"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.mtc-nic.id, # use network interface id
+  ]
+
+  # install docker on the linux box
+  custom_data = filebase64("customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/mtcazurekey.pub") # use ssh key generated on command line
+  }
+  # ssh into the box with ssh -i ~/.ssh/mtcazurekey adminuser@<public ip>
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  # use linux or windows template file to allow vs code to connect to box via ssh
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-script.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "adminuser",
+      identityFile = "~/.ssh/mtcazurekey"
+    })
+    #interpreter = ["Powershell", "-Command"]
+    #interpreter = ["bash", "-c"]
+    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+data "azurerm_public_ip" "mtc-ip-data" {
+  name                = azurerm_public_ip.mtc-ip.name
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+}
+
+# run terraform output to see this output
+output "public_ip_address" {
+  value = "${azurerm_linux_virtual_machine.mtc-vm.name}: ${data.azurerm_public_ip.mtc-ip-data.ip_address}"
+}
+
+#terraform state list or terraform show <resource> to show resources
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "host_os" {
+  type        = string
+  default     = "linux"
+  description = "OS specific var to allow vs code to ssh into linux box"
+}
